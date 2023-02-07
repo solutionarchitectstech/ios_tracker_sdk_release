1f545ba4b5919f7adedee94c2eb668fe35b13859
Radch-enko <stas.radchenko@effective.band>
2023-02-07 19:01:57 +0600

    Prepared release v1.2.0

---
+++ SATrackingSDKStandardExample/SATrackingSDKStandardExample/AppDelegate.swift
@@ -0,0 +1,65 @@
+//
+// Copyright © Gusev Andrew, Emelianov Andrew, Spinov Dmitry [collectively referred as the Authors], 2017 - All Rights Reserved.
+// [NOTICE: All information contained herein is, and remains the property of the Authors.]
+// The intellectual and technical concepts contained herein are proprietary to the Authors
+// and may be covered by any existing patents of any country in the world, patents in
+// process, and are protected by trade secret or copyright law.
+//
+// Dissemination of this information or reproduction of this material is strictly forbidden unless prior
+// written permission is obtained from the Authors. Access to the source code contained herein is hereby forbidden
+// to anyone except persons (natural person, corporate or unincorporated body, whether or not having a separate
+// legal personality, and that person’s personal representatives, and successors)
+// the Authors have granted permission and who have executed Confidentiality and Non-disclosure agreements
+// explicitly covering such access.
+//
+// The copyright notice above does not provide evidence of any actual or intended publication or disclosure
+// of this source code, which in
+//
+
+import UIKit
+import SATrackingSDKStandard
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        // Let's initialize SATrackingSDKStandard
+        TechTracker.initialize(options: TrackerOptions(
+            partnerId: "YOUR_PARTNER_ID",
+            uid: "YOUR_UID",
+            endpointUrl: "https://YOUR_ENDPOINT",
+            debugMode: true,
+            httpHeaders: [
+                "Authorization": "YOUR_AUTHORIZATION_TOKEN"
+            ]
+        ))
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+}
